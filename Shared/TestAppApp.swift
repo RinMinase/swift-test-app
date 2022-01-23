4e1bf16e99e8fddaf44f38b80864a923f40aa36c
Rin Minase <rin.black.raison.detre@gmail.com>
2022-01-23 21:41:28 +0800

    Appended initial files

---
+++ Shared/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  Shared
+//
+//  Created by Rin Minase on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
